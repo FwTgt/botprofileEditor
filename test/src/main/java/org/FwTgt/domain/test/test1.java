cf33a0cb6b3a16e700f80287414a11c68ad0b3c1
upczsh <1393472565@qq.com>
2021-04-14 13:29:55 +0800

    test this test11

---
+++ test/src/main/java/org/FwTgt/domain/test/test1.java
@@ -0,0 +1,18 @@
+package org.FwTgt.domain.test;
+
+
+import org.junit.jupiter.api.Test;
+
+
+public class test1 {
+        @Test
+    public  void test11()
+    {
+        String s1 = new String("11111");
+        System.out.println(s1);
+
+    }
+
+
+
+}
