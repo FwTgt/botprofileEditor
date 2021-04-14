a8f7f741ba1ba1ee82cbfefa896c4b62025a3057
ca11m3g00d <callmegood@yeah.net>
2021-04-14 12:46:20 +0800

    Bot.java 的设计已完成

--- domain/src/main/java/org/FwTgt/domain/Bot.java
+++ domain/src/main/java/org/FwTgt/domain/Bot.java
@@ -3,5 +3,40 @@ package org.FwTgt.domain;
 import java.util.List;
 
 public class Bot {
+    private String id;
+    private String name;
+    private Difficulty difficulty;
+    private Location location;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
 }
